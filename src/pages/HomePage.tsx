df09c0a3953ef4071ade946a01b1bed05fc6371b
nhassl3 <matvey.kvasov.05@mail.ru>
2026-08-17 15:45:50 +0300

    ➖ 🔥 Remove BlueprintBackgorund from imports and delete him in code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,18 @@
-import { useEffect } from 'react';
-import { BlueprintBackground } from '../components/layout/BlueprintBackground';
-import { Nav } from '../components/layout/Nav';
-import { Footer } from '../components/layout/Footer';
-import { Hero } from '../components/home/Hero';
-import { Services } from '../components/home/Services';
-import { Cases } from '../components/home/Cases';
-import { Stats } from '../components/home/Stats';
-import { Certs } from '../components/home/Certs';
-import { Partners } from '../components/home/Partners';
-import { CTA } from '../components/home/CTA';
-import { QuoteModal } from '../components/home/QuoteModal';
-import { CursorBlob } from '../components/shared/CursorBlob';
+import { useEffect } from 'react'
+import { Cases } from '../components/home/Cases'
+import { Certs } from '../components/home/Certs'
+import { CTA } from '../components/home/CTA'
+import { Hero } from '../components/home/Hero'
+import { Partners } from '../components/home/Partners'
+import { QuoteModal } from '../components/home/QuoteModal'
+import { Services } from '../components/home/Services'
+import { Stats } from '../components/home/Stats'
+import { Footer } from '../components/layout/Footer'
+import { Nav } from '../components/layout/Nav'
+import { useScrollReveal } from '../hooks/useScrollReveal'
+import { useAppSelector } from '../store/hooks'
 import { useBackgroundEffects } from '../hooks/useBackgroundEffects';
-import { useScrollReveal } from '../hooks/useScrollReveal';
-import { useAppSelector } from '../store/hooks';
+import { CursorBlob } from ' ../components/shared/CursorBlob';
 
 export function HomePage() {
   useBackgroundEffects();
@@ -29,7 +28,6 @@ export function HomePage() {
   return (
     <>
       <div className={quoteOpen ? 'app-blur' : ''}>
-        <BlueprintBackground />
         <Nav />
         <main>
           <Hero />
