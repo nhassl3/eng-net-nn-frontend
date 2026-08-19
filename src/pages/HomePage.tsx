af2674062d0d7dc18ab097b29b97e8b1e9ebfeb4
nhassl3 <matvey.kvasov.05@mail.ru>
2026-08-19 13:58:41 +0300

    ⚡ Provided CertsModal to HomePage for rendering due changes in 56bb6b3

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { Cases } from '../components/home/Cases'
 import { Certs } from '../components/home/Certs'
+import { CertsModal } from '../components/home/CertsModal'
 import { CTA } from '../components/home/CTA'
 import { Hero } from '../components/home/Hero'
 import { Partners } from '../components/home/Partners'
@@ -17,6 +18,7 @@ export function HomePage() {
   useScrollReveal();
   const quoteOpen = useAppSelector((s) => s.modal.quoteOpen);
   const serviceIx = useAppSelector((s) => s.modal.serviceIx);
+  const certsIdx = useAppSelector((s) => s.modal.certsIdx);
 
   useEffect(() => {
     document.documentElement.setAttribute('data-style', 'b');
@@ -26,7 +28,7 @@ export function HomePage() {
 
   return (
     <>
-      <div className={quoteOpen || serviceIx ? 'app-blur' : ''}>
+      <div className={quoteOpen || serviceIx || certsIdx ? 'app-blur' : ''}>
         <Nav />
         <main>
           <Hero />
@@ -41,6 +43,7 @@ export function HomePage() {
       </div>
       <QuoteModal />
       <ServicesModal />
+      <CertsModal />
     </>
   );
 }
