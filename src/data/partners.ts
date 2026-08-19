1aa23383ad2b5a402669cf61316d1667807fce0c
nhassl3 <matvey.kvasov.05@mail.ru>
2026-08-19 17:30:58 +0300

    ♻ Refactors in data
    Updated interface for logo, slug and url; Added this logos and urls

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,19 +1,21 @@
 export interface Partner {
   name: string;
-  mark: string;
+  slug: string;
+  logo?: string;
+  url: string;
 }
 
 export const PARTNER_BRANDS: Partner[] = [
-  { name: 'НОВАТЭК', mark: 'N' },
-  { name: 'СИБУР', mark: 'S' },
-  { name: 'ГК ПИК', mark: 'П' },
-  { name: 'ЛУКОЙЛ', mark: 'L' },
-  { name: 'РЖД', mark: 'Р' },
-  { name: 'РОСАТОМ', mark: 'A' },
-  { name: 'ГАЗПРОМ', mark: 'G' },
-  { name: 'СЕВЕРСТАЛЬ', mark: 'C' },
-  { name: 'МЕТАЛЛОИНВЕСТ', mark: 'M' },
-  { name: 'ОМК', mark: 'O' },
-  { name: 'УЗТМ', mark: 'У' },
-  { name: 'ЕВРАЗ', mark: 'E' },
+  { name: 'НОВАТЭК', slug: 'novatek', logo: '/partners/novatek.svg', url: 'https://novatek.ru' },
+  { name: 'СИБУР', slug: 'sibur', logo: '/partners/sibur.svg', url: 'https://www.sibur.ru' },
+  { name: 'ГK ПИК', slug: 'pik', logo: '/partners/pik.svg', url: 'https://www.pik.ru' },
+  { name: 'ЛУКОЙЛ', slug: 'lukoil', logo: '/partners/lukoil.svg', url: 'https://lukoil.ru' },
+  { name: 'РЖД', slug: 'rzd', logo: '/partners/rzd.svg', url: 'https://www.rzd.ru' },
+  { name: 'РОСАТОМ', slug: 'rosatom', logo: '/partners/rosatom.svg', url: 'https://rosatom.ru' },
+  { name: 'ГАЗПРОМ', slug: 'gazprom', logo: '/partners/gazprom.svg', url: 'https://www.gazprom.ru' },
+  { name: 'СЕВЕРСТАЛЬ', slug: 'severstal', logo: '/partners/severstal.png', url: 'https://severstal.com' },
+  { name: 'МЕТАЛЛОИНВЕСТ', slug: 'metalloinvest', logo: '/partners/metalloinvest.svg', url: 'https://www.metalloinvest.com' },
+  { name: 'ОМК', slug: 'omk', logo: '/partners/omk.svg', url: 'https://www.omk.ru' },
+  { name: 'УЗТМ', slug: 'uztm', logo: '/partners/uztm.png', url: 'https://www.uralmash.ru' },
+  { name: 'ЕВРАЗ', slug: 'evraz', logo: '/partners/evraz.svg', url: 'https://evraz.com' },
 ];
